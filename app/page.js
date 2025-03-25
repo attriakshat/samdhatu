38c9441bf0a876c1bb33cb8a048b0900a1caeeef
attriakshat <166594767+attriakshat@users.noreply.github.com>
2025-03-25 11:04:56 +0530

    Addition of scss and meta tags

--- app/page.js
+++ app/page.js
@@ -1,95 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from 'next/link';
+import styles from './page.module.scss';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<h1>The Ayurvedic Philosophy</h1>
+			<h2>The Human body</h2>
+			<p>
+				Ayurveda suggests that the human body is an amalgamation of the root
+				elemnts that make up our universe, It suggests that we are not just
+				material beings but also energies. It is believed that the state of
+				balance or imbalance of these root elements directly affects the state
+				of your health. The state of balance which is believed to be life giving
+				is knows as <Link href={'/'}>Samdhatu</Link>
+			</p>
+			<h2>What are the root elements?</h2>
+			<p>
+				The root elements are Akash(space), Vayu (air), Agni(Fire/Heat),
+				Jal(water/liquid) and prathvi(solid matter/ earth). It is believed that
+				the elements of space and earth are in a stable state in our body. Only
+				the elements Air, Fire and Water are in motion, in other words can
+				change. When these elements are disbalanced they are called Doshas, and
+				this imbalance destorys the inner working of our bodies causing diseases
+				and the diseases are merly symptoms of the existing disbalance.
+			</p>
+		</div>
+	);
 }
