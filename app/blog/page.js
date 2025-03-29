0e7f21fa822a6a49013e7fa3b7321e29b2f4bb50
attriakshat <166594767+attriakshat@users.noreply.github.com>
2025-03-29 12:02:32 +0530

    added scss files with theme colors and moved blog content to blog page, on the home page svg practice code has been added

---
+++ app/blog/page.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import styles from '../page.module.scss';
+
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<h1>The Ayurvedic Philosophy</h1>
+			<h2>The Human body</h2>
+			<p>
+				Ayurveda suggests that the human body is an amalgamation of the root
+				elemnts that make up our universe, It suggests that we are not just
+				material beings but also energies. It is believed that the state of
+				balance or imbalance of these root elements directly affects the state
+				of your health. The state of balance which is believed to be life giving
+				is knows as <Link href={'/'}>Samdhatu</Link>
+			</p>
+			<h2>What are the root elements?</h2>
+			<p>
+				The root elements are Akash(space), Vayu (air), Agni(Fire/Heat),
+				Jal(water/liquid) and prathvi(solid matter/ earth). It is believed that
+				the elements of space and earth are in a stable state in our body. Only
+				the elements Air, Fire and Water are in motion, in other words can
+				change. When these elements are disbalanced they are called Doshas, and
+				this imbalance destorys the inner working of our bodies causing diseases
+				and the diseases are merly symptoms of the existing disbalance.
+			</p>
+		</div>
+	);
+}
--- app/page.js
+++ app/page.js
@@ -1,29 +1,148 @@
-import Link from 'next/link';
 import styles from './page.module.scss';
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
-			<h1>The Ayurvedic Philosophy</h1>
-			<h2>The Human body</h2>
-			<p>
-				Ayurveda suggests that the human body is an amalgamation of the root
-				elemnts that make up our universe, It suggests that we are not just
-				material beings but also energies. It is believed that the state of
-				balance or imbalance of these root elements directly affects the state
-				of your health. The state of balance which is believed to be life giving
-				is knows as <Link href={'/'}>Samdhatu</Link>
-			</p>
-			<h2>What are the root elements?</h2>
-			<p>
-				The root elements are Akash(space), Vayu (air), Agni(Fire/Heat),
-				Jal(water/liquid) and prathvi(solid matter/ earth). It is believed that
-				the elements of space and earth are in a stable state in our body. Only
-				the elements Air, Fire and Water are in motion, in other words can
-				change. When these elements are disbalanced they are called Doshas, and
-				this imbalance destorys the inner working of our bodies causing diseases
-				and the diseases are merly symptoms of the existing disbalance.
-			</p>
+		<div className={styles.svgContainer}>
+			<svg
+				width='200'
+				height='200'
+			>
+				<circle
+					cx='100'
+					cy='120'
+					r='70'
+					fill='#a4b465'
+				/>
+				<rect
+					x='82'
+					y='35'
+					width='35'
+					height='20'
+					fill='#ffcf50'
+				/>
+				<circle
+					cx='100'
+					cy='25'
+					r='12'
+					fill='none'
+					stroke='#F79257'
+					stroke-width='2'
+				/>
+			</svg>
+
+			<svg
+				width='200'
+				height='400'
+				viewbox='-100 -200 200 400'
+			>
+				<polygon
+					points='0,0 80,120 -80,120'
+					fill='#234236'
+				/>
+				<polygon
+					points='0,-40 60,60 -60,60'
+					fill='#0C5C4C'
+				/>
+				<polygon
+					points='0,-80 40,0 -40,0'
+					fill='#38755B'
+				/>
+				<rect
+					width='40'
+					height='30'
+					fill='brown'
+					x='-20'
+					y='120'
+				/>
+			</svg>
+			<svg
+				width='200'
+				height='200'
+				viewbox='-100 -100 200 200'
+			>
+				<circle
+					className={styles.head}
+					cx='0'
+					cy='-50'
+					r='30'
+				/>
+				<circle
+					class={styles.eye}
+					cx='-12'
+					cy='-55'
+					r='3'
+				/>
+				<circle
+					class={styles.eye}
+					cx='12'
+					cy='-55'
+					r='3'
+				/>
+				<rect
+					className={styles.mouth}
+					x='-10'
+					y='-40'
+					width='20'
+					height='5'
+					rx='2'
+				/>
+				<line
+					className={styles.limb}
+					x1='-40'
+					y1='-10'
+					x2='40'
+					y2='-10'
+				/>
+				<line
+					className={styles.limb}
+					x1='-25'
+					y1='50'
+					x2='0'
+					y2='-15'
+				/>
+				<line
+					className={styles.limb}
+					x1='25'
+					y1='50'
+					x2='0'
+					y2='-15'
+				/>
+				<circle
+					cx='0'
+					cy='-10'
+					r='5'
+				/>
+				<circle
+					cx='0'
+					cy='10'
+					r='5'
+				/>
+			</svg>
+			<svg
+				width='200'
+				height='200'
+				viewbox='-100 -100 200 200'
+			>
+				<path
+					className={styles.hamburgerRow}
+					d=' M -50, -25 
+						L -50, -50
+						L -25, -50
+						M 25, -50
+						L 50, -50
+						L 50, -25
+						M 50, 25
+						L 50, 50
+						L 25, 50
+						M -25, 50
+						L -50, 50
+						L -50, 25'
+				/>
+			</svg>
+			<svg
+				width='450'
+				height='450'
+			></svg>
 		</div>
 	);
 }
